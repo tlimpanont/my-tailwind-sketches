b049c4f464bdde188badf4b5205aefe65345617b
Theuy Limpanont <info@theuy.nl>
2021-09-19 22:19:52 +0200

    fix(combobox.stories): correct typings

--- src/stories/ComboBox/ComboBox.stories.tsx
+++ src/stories/ComboBox/ComboBox.stories.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ComboBox, {ComboBoxProps} from "../../components/ComboBox";
-import {ComponentStory} from "@storybook/react";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
 
 export default {
     title: "grommet/ComboBox",
     component: ComboBox
-}
+} as ComponentMeta<typeof ComboBox>;
 
-export const Default: ComponentStory<typeof ComboBox> = (props: ComboBoxProps) => <ComboBox {...props} />;
+const Template: ComponentStory<typeof ComboBox> = (props: ComboBoxProps) => <ComboBox {...props} />;
+
+export const Default = Template.bind({})
+
 Default.parameters = {
     layout: "centered"
 }
